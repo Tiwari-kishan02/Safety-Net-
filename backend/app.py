b042b9a74a453c23a91f75319a329afb3af7baa7
Tiwari-kishan02 <tiwarikishan702@gmail.com>
2026-07-20 23:45:07 +0530

    Added Column D -Hacked Account Recovery Assitant page

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "SafetyNet+ backend is running!"
 
+@app.route('/recovery')
+def recovery():
+    return render_template('recovery.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
